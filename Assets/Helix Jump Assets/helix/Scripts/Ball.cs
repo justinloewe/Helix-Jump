bf70956f2cd64b3c42802d75dcb6df632f6f961d
Justin Löwe <justin.loewe@r132-05-w10.SCHULE-ISERV>
2018-11-01 14:37:51 +0100

    <1.11.18>

---
+++ Assets/Helix Jump Assets/helix/Scripts/Ball.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	public GameObject Splash_Prefab;
+
+	public float force;
+
+	void OnCollisionEnter(Collision col){
+
+		if (col.gameObject.tag == "h_piece") {
+			gameObject.GetComponent<Rigidbody> ().AddForce (new Vector3 (0f, 1f, 0f) * Time.deltaTime * force);
+			GameObject splash = Instantiate (Splash_Prefab);
+			splash.transform.SetParent (GameObject.Find("helix").transform);
+		}
+
+	}
+		
+}
+
